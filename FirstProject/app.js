341dcb8a47353a9097c7f461d2215732be32a6b5
ilketorun <ilketorun97@gmail.com>
2022-10-24 12:06:56 +0300

    Core Types: Object, Array, Tuples

--- FirstProject/app.js
+++ FirstProject/app.js
@@ -12,3 +12,20 @@ var number2 = 2.7;
 var showResult = true;
 var phrase = 'The Result is: ';
 var result = add(number1, number2, showResult, phrase);
+var person = {
+    name: 'ilke',
+    age: 25,
+    hobbies: ['Sports', 'Cooking'],
+    role: ['String', 20]
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+var newTuple = ['as', 20];
+// newTuple[1] = 'asd'; // -> Not allowed, type checked
+// newTuple = ['asd',20,10] // -> Not allowed, length checked
+newTuple.push('admin'); // -> allowed, This is the down side
+console.log(newTuple);
--- FirstProject/app.ts
+++ FirstProject/app.ts
@@ -7,9 +7,36 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     }
 }
 
-const number1: number = 5;
-const number2: number = 2.7;
-const showResult: boolean = true
-const phrase: string = 'The Result is: '
+const number1 = 5;
+const number2 = 2.7;
+const showResult = true
+const phrase = 'The Result is: '
 
 const result = add(number1,number2, showResult, phrase);
+
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [string, number];
+} = {
+    name: 'ilke',
+    age: 25,
+    hobbies: ['Sports', 'Cooking'],
+    role: ['String', 20] 
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
+
+let newTuple: [string, number] = ['as', 20]
+// newTuple[1] = 'asd'; // -> Not allowed, type checked
+// newTuple = ['asd',20,10] // -> Not allowed, length checked
+newTuple.push('admin') // -> allowed, This is the down side
+
+console.log(newTuple);
