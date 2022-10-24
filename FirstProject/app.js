e0a1a634611585852ac1e2d17ac37b9978695f33
ilketorun <ilketorun97@gmail.com>
2022-10-24 10:36:58 +0300

    Core types: number, string and boolean

--- FirstProject/app.js
+++ FirstProject/app.js
@@ -1,2 +1,14 @@
-console.log('This is the first project...');
-
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.7;
+var showResult = true;
+var phrase = 'The Result is: ';
+var result = add(number1, number2, showResult, phrase);
--- FirstProject/app.ts
+++ FirstProject/app.ts
@@ -1,1 +1,15 @@
-console.log('This is the first project...');
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result: number = n1 + n2
+    if(showResult){
+        console.log(phrase + result)
+    } else {
+        return result;
+    }
+}
+
+const number1: number = 5;
+const number2: number = 2.7;
+const showResult: boolean = true
+const phrase: string = 'The Result is: '
+
+const result = add(number1,number2, showResult, phrase);
